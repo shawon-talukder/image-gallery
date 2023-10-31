e01a1de78564dd563737b6fd3efdad653bb1ab2b
shawonTalukder <shawontalukder.study@gmail.com>
2023-11-01 01:30:04 +0600

    chore: create model, schema for image

---
+++ backend/model/image.js
@@ -0,0 +1,32 @@
+/*
+ *
+ *
+ ------->Title: image Schema & Model
+ ->Description: this file is to handle schema related to image, structuring model
+ ------>Author: Shawon Talukder
+ -------->Date: 11/01/2023
+ *
+ *
+ */
+
+ // Dependencies
+ const mongoose = require("mongoose");
+ 
+ // schema structure
+ const imageSchema = new mongoose.Schema({
+    position:{
+        type: Number, 
+        required:true
+    },
+    imageUrl:{
+        type:String, 
+        required:true
+    },
+ },
+ { timestamps:true });
+ 
+ // Model Structure
+ const Image = mongoose.model("image", imageSchema);
+
+ // Export Model
+ module.exports = Image
