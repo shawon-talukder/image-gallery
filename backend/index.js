558ec286a8bc9ae561851af6bfb8f60805444e8f
shawonTalukder <shawontalukder.study@gmail.com>
2023-11-01 00:12:50 +0600

    chore: basic project setup

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+// dependencies
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+
+// internal dependencies
+
+// model scaffolding
+const app = express();
+
+// configuration
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("short"));
+
+// routes
+
+
+// server port
+app.listen(port, ()=>{
+    console.log(`app listening on port: http://localhost:${port}`);
+})
