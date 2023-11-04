e3359b3d7953a57d6e144c9784282de4698ada4e
shawonTalukder <shawontalukder.study@gmail.com>
2023-11-04 23:17:22 +0600

    database connection

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const morgan = require("morgan");
-const imageRouter = require("./routes/imageRouter");
+const mongoose = require("mongoose");
 
 // internal dependencies
+const imageRouter = require("./routes/imageRouter");
 
 // model scaffolding
 const app = express();
@@ -22,6 +23,12 @@ app.use(morgan("short"));
 
 // routes
 app.use("/api/v1/image", imageRouter)
+
+// connect to database
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>{console.log("database connected successfully!")})
+    .catch((err)=>{console.log("database connection failed!", err)});
 
 // server port
 app.listen(port, ()=>{
