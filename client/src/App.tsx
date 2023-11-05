054b5bcb1e33e7a13b87784dd1a0585dbbc376a9
shawonTalukder <shawontalukder.study@gmail.com>
2023-11-06 00:03:53 +0600

    chore: products item, demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+import ProductList from "./components/ProductList";
 import Topbar from "./components/Topbar";
 
 function App() {
   return (
-    <div className="bg-blue-500/40 h-screen w-screen flex justify-center items-center shadow-sm">
+    <div className="bg-blue-500/40 h-screen w-full flex justify-center items-center shadow-sm">
       <div className="w-11/12 md:w-3/4 flex flex-col gap-[2px]">
         <Topbar />
+        <ProductList />
       </div>
     </div>
   );
---
+++ client/src/components/ProductList.tsx
@@ -0,0 +1,22 @@
+const ProductList = () => {
+  return (
+    <div className="bg-white rounded-b px-8 md:px-12 py-4 md:py-8 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-2 md:gap-4">
+      <div className="bg-black col-span-2 row-span-2 min-h-[100px] rounded">
+        hello
+      </div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-black min-h-[100px] rounded">hello</div>
+      <div className="bg-gray-500/20 min-h-[100px] rounded flex justify-center items-center">
+        add image
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
