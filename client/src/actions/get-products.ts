9e60a3f09baf638e00bb509fd9c2ee4ff400c08b
shawonTalukder <shawontalukder.study@gmail.com>
2023-11-06 12:46:40 +0600

    fix: converting data was not correct, since axios returns resolved data as data

--- client/src/actions/get-products.ts
+++ client/src/actions/get-products.ts
@@ -1,18 +1,21 @@
 import axios from "../lib/axiosInstance";
-
+export interface IProduct {
+  _id: string;
+  imageUrl: string;
+  position: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
 const getProducts = async () => {
-  let error,
-    products,
-    isError = false;
   try {
-    products = await axios.get("/");
-  } catch (err) {
-    console.log("ACTIONS/GET_PRODUCTS", err);
-    error = err;
-    isError = true;
-  }
+    const products: IProduct[] = (await axios.get("/")).data.images;
 
-  return { isError, error, products };
+    return { isError: false, error: "", products: products };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    console.log("ACTIONS/GET_PRODUCTS", err);
+    return { isError: true, error: err.message, products: [] };
+  }
 };
 
 export default getProducts;
