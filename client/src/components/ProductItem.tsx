321150430467bfe2d57372f8985d62c08d82f428
shawonTalukder <shawontalukder.study@gmail.com>
2023-11-06 20:45:29 +0600

    chore: all functionality working fine

--- client/src/components/ProductItem.tsx
+++ client/src/components/ProductItem.tsx
@@ -1,32 +1,53 @@
+import useStore from "../hooks/useStore";
+
 interface ProductItemType {
+  id: string;
   imageUrl: string;
   position: number;
 }
 
-const ProductItem = ({ imageUrl, position }: ProductItemType) => {
-  let imageContent;
-  if (position === 1) {
-    imageContent = (
-      <div className="bg-gray-500/20 col-span-2 row-span-2 min-h-[200px] min-w-[200px]  rounded">
-        <img
-          className="object-contain w-32 h-32"
-          src={imageUrl}
-          alt={imageUrl}
-        />
-      </div>
-    );
-  } else {
-    imageContent = (
-      <div className="bg-gray-500/20 min-h-[100px] rounded flex justify-center items-center">
-        <img
-          className="object-contain w-20 h-20"
-          src={imageUrl}
-          alt={imageUrl}
-        />
-      </div>
-    );
-  }
-  return <>{imageContent}</>;
+const ProductItem = ({ id, imageUrl, position }: ProductItemType) => {
+  const { checkedImg, removeCheckedImg, updateCheckedImg } = useStore();
+
+  const handleCheck = () => {
+    if (checkedImg.includes(id)) {
+      removeCheckedImg(id);
+    } else {
+      updateCheckedImg(id);
+    }
+  };
+
+  const checkedOnLeftTop = checkedImg.includes(id) && (
+    <input
+      className={`absolute top-2 left-2 bg-green-200 group-hover:hidden ${
+        position === 1 ? "w-6 h-6" : "w-4 h-4"
+      }`}
+      type="checkbox"
+      defaultChecked={checkedImg.includes(id)}
+    />
+  );
+  return (
+    <div
+      className={`${
+        position === 1
+          ? "col-span-2 row-span-2 min-h-[200px]"
+          : "col-span-1 min-h-[100px]"
+      } group bg-gray-500/20 flex justify-center items-center rounded relative hover:opacity-70 cursor-pointer`}
+    >
+      {checkedOnLeftTop}
+      <img
+        className={`object-contain w-3/4 ${position === 1 ? "h-32" : "h-20"}`}
+        src={imageUrl}
+        alt={imageUrl}
+      />
+      <input
+        type="checkbox"
+        className="absolute hidden group-hover:block w-6 h-6 cursor-pointer"
+        checked={checkedImg.includes(id)}
+        onChange={handleCheck}
+      />
+    </div>
+  );
 };
 
 export default ProductItem;
